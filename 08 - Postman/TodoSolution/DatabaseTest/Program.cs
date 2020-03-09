2c05a6662ac155839ebffdfd92dbe867ef49dc07
John Kocer <john.kocer@gmail.com>
2020-03-09 16:08:07 -0700

    Added EmployeeController for Postman testing

---
+++ 08 - Postman/TodoSolution/DatabaseTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using SmartIT.DebugHelper;
+using SmartIT.Employee.MockDB;
+
+namespace DatabaseTest
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      TodoRepository _todoRepository = new TodoRepository();
+      var todoList = _todoRepository.GetAll();
+      todoList.CDump("_todoRepository.GetAll()");
+      var findById = _todoRepository.FindById(2);
+      findById.CDump("_todoRepository.FindById(2)");
+      var newTodo = _todoRepository.Add(new Todo { Name = "Call a friend" });
+      _todoRepository.GetAll().CDump("Check if Call a friend todo added?");
+      newTodo.Name = newTodo.Name + " Updated";
+      _todoRepository.Update(newTodo);
+      _todoRepository.GetAll().CDump("Check if Call a friend todo updated with Updated?");
+      _todoRepository.Delete(_todoRepository.FindById(1));
+      _todoRepository.GetAll().CDump("Check if Id=1 todo is Deleted?");
+
+      Console.ReadLine();
+    }
+  }
+}
---
+++ 08 - Postman/TodoSolution/Todo.Mvc.Ui/Models/WorkAround.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Mvc.Ui.Models
+{
+  public class Todo : SmartIT.Employee.MockDB.Todo { }
+}
